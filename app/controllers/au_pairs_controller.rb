a9d6d3cf636351f1a1b8373371c015a8e2354dd3
Anteo Guillot <anteo.guillot@gmail.com>
2017-02-21 19:41:11 +0100

    added a view for au_pair index, deleted attr_accessor that fucked up the email persist

--- app/controllers/au_pairs_controller.rb
+++ app/controllers/au_pairs_controller.rb
@@ -1,8 +1,11 @@
 class AuPairsController < ApplicationController
 
   def index
-  @aupair = User.au_pair
+    @aupairs = User.au_pair
   end
 
+  def show
+    @aupair = User.find(params[:id])
+  end
 
 end
