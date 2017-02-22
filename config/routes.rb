f5e498359ea54c5b52883ea4ee9fd97b5a2fad44
macbook <petitm31@gmail.com>
2017-02-22 12:02:40 +0100

    Add au pair show and css

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :au_pairs, only: [:index, :show]
-  resources :bookings
+  resources :au_pairs, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
