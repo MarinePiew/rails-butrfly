894509206a5250ee93307ef66b919c5462f9e6cc
Thibault Lanery <thibaultlanery@gmail.com>
2017-02-24 15:00:49 -0800

    modif controller de aupairs index

--- app/controllers/au_pairs_controller.rb
+++ app/controllers/au_pairs_controller.rb
@@ -2,14 +2,19 @@ class AuPairsController < ApplicationController
 skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    gender = params[:search][:gender]
-    nationality = params[:search][:nationality]
-    @aupairs = User.where(gender: gender, nationality: nationality)
+    @aupairs = User.au_pair
+    @aupairs = @aupairs.where(search_params) if params[:search]
   end
 
   def show
     @aupair = User.find(params[:id])
     @review = Review.new
+  end
+
+  private
+
+  def search_params
+    params.require(:search).permit(:gender, :nationality)
   end
 
 end
