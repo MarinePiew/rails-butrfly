689519e39daf77cc7c883085d1cf54a729943f23
macbook <petitm31@gmail.com>
2017-02-24 09:33:52 +0100

    Add reviews columns

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,4 @@
 class Booking < ApplicationRecord
   belongs_to :family, foreign_key: :family_id, class_name: 'User'
   belongs_to :au_pair, foreign_key: :au_pair_id, class_name: 'User'
-   has_many :reviews, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :bookings, foreign_key: :family_id, class_name: 'Booking'
   has_many :au_pair_bookings, foreign_key: :au_pair_id, class_name: 'Booking'
-
+  has_many :reviews, dependent: :destroy
 
   mount_uploader :picture, PhotoUploader
 
